b7ab17ea60dc823143b955b525a1922a81a0a4aa
Dmitry <budyachocheck@gmail.com>
2014-12-27 05:30:53 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NeeyAppp::Application.config.secret_key_base = 'b6d0df9798a941468b01f3407a7be8c4c577ee31550957102ac38787041bc5c673f55c0fcbeea90abb6145bd48d4a70f04ae7e588abadbb58323086d54375175'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NeeyAppp::Application.config.session_store :cookie_store, key: '_neey_appp_session'
